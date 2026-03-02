690505fc2797636fd471d82ac31a790fbe7d4024
Alejandro Navarro <100772082+Navapu@users.noreply.github.com>
2026-03-02 17:18:27 +0100

    feat: connect project to mongoDB

--- config/config.js
+++ config/config.js
@@ -7,3 +7,11 @@ dotenv.config();
 export const PORT = process.env.PORT;
 export const BACKEND_URL = process.env.BACKEND_URL;
 export const NODE_ENV = process.env.NODE_ENV;
+
+// ---------------------------
+// BBDD
+// ---------------------------
+export const DB_USER = process.env.DB_USER || "user";
+export const DB_PASS = process.env.DB_PASS || "1234";
+export const CLUSTER = process.env.CLUSTER || "cluster.mongodb.net";
+export const DATABASE = process.env.DATABASE || "database";
--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 import express from 'express';
 import { PORT, BACKEND_URL} from './config/config.js';
+import { connectDB } from './db/mongoose.js';
 const app = express();
 
+
+// ---------------------------
+// MIDDLEWARES / MIDDLEWARES
+// ---------------------------
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+connectDB();
 
 app.get('/', (req, res) => {
-    res.send("Vinted Tracker API")
+    res.send("Profit Tracker API")
 })
 
 app.listen(PORT, () => {
