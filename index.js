b485070b319670de82cce23b81420f0e9b2f427b
Alejandro Navarro <100772082+Navapu@users.noreply.github.com>
2026-03-02 17:26:57 +0100

    chore: add pino logger for structured logging

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import { PORT, BACKEND_URL} from './config/config.js';
 import { connectDB } from './db/mongoose.js';
+import logger from "./config/logger.js";
+
 const app = express();
 
 
@@ -17,5 +19,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(PORT, () => {
-    console.log(`Server running at: ${BACKEND_URL}${PORT}`)
+    logger.info(`Server running at: ${BACKEND_URL}${PORT}`)
 })
