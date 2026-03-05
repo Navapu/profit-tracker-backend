c3b1b0fe5e11fbfb0a2f94b4ff93b36f4e113a29
Alejandro Navarro <100772082+Navapu@users.noreply.github.com>
2026-03-05 17:31:30 +0100

    feat(transactions): add summary endpoint for income, expense and balance

--- controllers/transaction.controller.js
+++ controllers/transaction.controller.js
@@ -1,6 +1,6 @@
 import logger from "../config/logger.js";
 import { Transaction } from "../db/models/index.js";
-
+import mongoose from "mongoose";
 const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
 export const createTransaction = async (req, res, next) => {
   try {
@@ -10,41 +10,45 @@ export const createTransaction = async (req, res, next) => {
 
     if (!type?.trim() || !name?.trim() || !amount || !transactionDate) {
       res.status(400);
-      return next(new Error("type, name, amount and transactionDate are required"));
-    };
-
-    if(type !== "income" && type !== "expense"){
-        res.status(400);
-        return next(new Error("type only can be 'income' or 'expense' "));
-    };
-
-    if(name?.trim().length < 3 || name?.trim().length > 100){
-        res.status(400);
-        return next(new Error("name length has to be higher than 3 and lower than 100"))
+      return next(
+        new Error("type, name, amount and transactionDate are required"),
+      );
     }
 
-    if(isNaN(Number(amount)) || Number(amount) <= 0){
-        res.status(400);
-        return next(new Error("amount must be a valid number and higher than 0"));
+    if (type !== "income" && type !== "expense") {
+      res.status(400);
+      return next(new Error("type only can be 'income' or 'expense' "));
+    }
+
+    if (name?.trim().length < 3 || name?.trim().length > 100) {
+      res.status(400);
+      return next(
+        new Error("name length has to be higher than 3 and lower than 100"),
+      );
+    }
+
+    if (isNaN(Number(amount)) || Number(amount) <= 0) {
+      res.status(400);
+      return next(new Error("amount must be a valid number and higher than 0"));
     }
 
     if (!dateRegex.test(transactionDate)) {
-        res.status(400);
-        return next(new Error("transactionDate must be in format YYYY-MM-DD"));
+      res.status(400);
+      return next(new Error("transactionDate must be in format YYYY-MM-DD"));
     }
-    
+
     const newTransaction = await Transaction.create({
-        userId,
-        type,
-        name,
-        amount,
-        transactionDate: new Date(transactionDate),
-        status: "completed"
+      userId,
+      type,
+      name,
+      amount,
+      transactionDate: new Date(transactionDate),
+      status: "completed",
     });
     return res.status(201).json({
-        msg: "New transaction created",
-        data: newTransaction,
-        error: false
+      msg: "New transaction created",
+      data: newTransaction,
+      error: false,
     });
   } catch (error) {
     logger.error(error, "createTransaction error:");
@@ -53,34 +57,37 @@ export const createTransaction = async (req, res, next) => {
 };
 
 export const getTransactions = async (req, res, next) => {
-  try{
+  try {
     const userId = req.user.id;
     const filter = { userId };
     const limit = Math.min(Math.max(Number(req.query.limit) || 10, 1), 100);
     const page = Math.max(Number(req.query.page) || 1, 1);
     const offset = (page - 1) * limit;
-    if(req.query.type) filter.type = req.query.type;
-    if(req.query.search) filter.name = {$regex: req.query.search, $options: "i"};    
-    if(req.query.status) filter.status = req.query.status;
-    
-    const sortOrder = req.query.order === "asc" ? 1 : -1
-        
+    if (req.query.type) filter.type = req.query.type;
+    if (req.query.search)
+      filter.name = { $regex: req.query.search, $options: "i" };
+    if (req.query.status) filter.status = req.query.status;
+
+    const sortOrder = req.query.order === "asc" ? 1 : -1;
+
     if (req.query.minAmount || req.query.maxAmount) {
       filter.amount = {};
       if (req.query.minAmount) filter.amount.$gte = Number(req.query.minAmount);
       if (req.query.maxAmount) filter.amount.$lte = Number(req.query.maxAmount);
     }
 
-    if(req.query.startDate || req.query.endDate){
+    if (req.query.startDate || req.query.endDate) {
       filter.transactionDate = {};
-      if(req.query.startDate) filter.transactionDate.$gte = new Date(req.query.startDate);
-      if(req.query.endDate) filter.transactionDate.$lte = new Date(req.query.endDate);
+      if (req.query.startDate) filter.transactionDate.$gte = new Date(req.query.startDate);
+      if (req.query.endDate) filter.transactionDate.$lte = new Date(req.query.endDate);
     }
-
 
     const totalTransactions = await Transaction.countDocuments(filter);
 
-    const transactions = await Transaction.find(filter).limit(limit).skip(offset).sort({transactionDate: sortOrder})
+    const transactions = await Transaction.find(filter)
+      .limit(limit)
+      .skip(offset)
+      .sort({ transactionDate: sortOrder });
 
     const totalPages = Math.ceil(totalTransactions / limit);
 
@@ -91,11 +98,63 @@ export const getTransactions = async (req, res, next) => {
         limit,
         page,
         totalPages,
-        transactions
-      }
+        transactions,
+      },
     });
-  }catch(error){
+  } catch (error) {
     logger.error(error, "getTransactions error:");
+    next(error);
+  }
+};
+
+export const getSummary = async (req, res, next) => {
+  try {
+    const userId = new mongoose.Types.ObjectId(req.user.id);
+    const filter = { userId };
+    if (req.query.startDate || req.query.endDate) {
+      filter.transactionDate = {};
+      if (req.query.startDate) filter.transactionDate.$gte = new Date(req.query.startDate);
+      if (req.query.endDate) filter.transactionDate.$lte = new Date(req.query.endDate);
+    };
+
+    const [summary] = await Transaction.aggregate([
+      { $match: filter },
+      {
+        $group: {
+          _id: "$userId",
+          totalIncome: {
+            $sum: {
+              $cond: [{ $eq: ["$type", "income"] }, "$amount", 0],
+            },
+          },
+          totalExpense: {
+            $sum: {
+              $cond: [{ $eq: ["$type", "expense"] }, "$amount", 0],
+            },
+          },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          totalIncome: 1,
+          totalExpense: 1,
+          balance: { $subtract: ["$totalIncome", "$totalExpense"] },
+        },
+      },
+    ]);
+    const result = summary || {
+      totalIncome: 0,
+      totalExpense: 0,
+      balance: 0,
+    };
+    res.status(200).json({
+      msg: "Summary obtained: ",
+      data: result,
+      error: false,
+    });
+  } catch (error) {
+    logger.error(error, "getSummary error:");
     next(error);
   }
 };
--- routes/transaction.routes.js
+++ routes/transaction.routes.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { createTransaction, getTransactions } from '../controllers/transaction.controller.js';
+import { createTransaction, getTransactions, getSummary } from '../controllers/transaction.controller.js';
 import { auth } from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
 
 router.get('/', auth, getTransactions);
 router.post('/create', auth, createTransaction);
-
+router.get('/summary', auth, getSummary);
 export default router;
